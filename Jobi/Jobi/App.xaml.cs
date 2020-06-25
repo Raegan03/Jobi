9ae2a580538af8fb3adc37b07dcbb0bc70e28cef
Janek <jan.gajewski3@gmail.com>
2020-06-25 20:17:53 +0200

    Init commit

---
+++ Jobi/Jobi/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Jobi.Services;
+using Jobi.Views;
+
+namespace Jobi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
